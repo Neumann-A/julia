3805dcfc7ad43a690b1024536263a39caacba622
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-05-22 16:14:00 +0200

    more cmake support

--- cmake/deps-setup.cmake
+++ cmake/deps-setup.cmake
@@ -2,14 +2,16 @@ include_guard(GLOBAL)
 
 include(FeatureSummary)
 
-set(JL_DEPS 
-        CSL LLVM PCRE LIBM LIBUNWIND DSFMT LIBBLASTRAMPOLINE BLAS LAPACK GMP MPFR
-        LIBSUITESPARSE LIBUV UTF8PROC MBEDTLS LIBSSH2 NGHTTP2 CURL LIBGIT2 PATCHELF
-        LIBWHICH ZLIB P7ZIP LLD
-    )
+#set(JL_DEPS 
+#        CSL LLVM PCRE LIBM LIBUNWIND DSFMT LIBBLASTRAMPOLINE BLAS LAPACK GMP MPFR
+#        LIBSUITESPARSE LIBUV UTF8PROC MBEDTLS LIBSSH2 NGHTTP2 CURL LIBGIT2 PATCHELF
+#        LIBWHICH ZLIB P7ZIP LLD
+#    )
 
-if(USE_SYSTEM_LLVM)
-    find_package(LLVM 15.0.7 COMPONENTS "") # which componens are required ?
+if(USE_SYSTEM_LLVM OR TRUE)
+    find_package(LLVM 15.0.7 COMPONENTS "LLVMDemangle;LLVMSupport;LLVMCodeGen;LLVMCore;LLVMX86CodeGen") # which componens are required ?
+    add_library(jl::llvm INTERFACE IMPORTED)
+    target_link_libraries(jl::llvm INTERFACE LLVMCore LLVMDemangle)
 else()
     # Vendor LLVM
 endif()
@@ -22,6 +24,18 @@ endif()
 
 # Setup dummy imported targets
 # TODO: Make these real imported targets
-add_library(jl::utf8proc IMPORTED UNKNOWN)
-add_library(jl::libuv IMPORTED UNKNOWN)
-add_library(jl::llvm IMPORTED UNKNOWN)
+find_path(UTF8PROC_INCLUDE_DIR NAMES utf8proc.h PATHS include)
+find_library(UTF8PROC_LIBRARY NAMES utf8proc PATHS lib)
+
+add_library(jl::utf8proc INTERFACE IMPORTED)
+target_include_directories(jl::utf8proc INTERFACE "${UTF8PROC_INCLUDE_DIR}")
+target_link_libraries(jl::utf8proc INTERFACE "${UTF8PROC_LIBRARY}")
+
+
+find_path(LIBUV_INCLUDE_DIR NAMES uv.h PATHS include)
+find_library(LIBUV_LIBRARY NAMES uv PATHS lib)
+
+add_library(jl::libuv INTERFACE IMPORTED)
+target_include_directories(jl::libuv INTERFACE "${LIBUV_INCLUDE_DIR}")
+target_link_libraries(jl::libuv INTERFACE "${LIBUV_LIBRARY}")
+
